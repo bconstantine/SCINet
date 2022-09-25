628cee54fc4fcf011ef3646da36a827dac036ff3
bconstantine <bryan.constantine7@gmail.com>
2022-09-25 18:52:27 +0800

    update

--- data_process/etth_data_loader.py
+++ data_process/etth_data_loader.py
@@ -228,9 +228,12 @@ class Dataset_Custom(Dataset):
             cols.remove('date')
         df_raw = df_raw[['date']+cols+[self.target]]
         print(f"len of df raw = {len(df_raw)}")
-        num_train = int(len(df_raw)*0.7)
-        num_test = int(len(df_raw)*0.2)
-        num_vali = len(df_raw) - num_train - num_test
+        #num_train = int(len(df_raw)*0.7)
+        #num_test = int(len(df_raw)*0.2)
+        #num_vali = len(df_raw) - num_train - num_test
+        num_test = 180
+        num_vali = 42
+        num_train = len(df_raw) - num_vali - num_test
         print(f"inside etth_data_loader train,test,vali: {num_train},{num_test}, {num_vali}")
         border1s = [0, num_train-self.seq_len, len(df_raw)-num_test-self.seq_len]
         border2s = [num_train, num_train+num_vali, len(df_raw)]
