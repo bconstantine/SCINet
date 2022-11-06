136113e67cb404e99f4ea5f5ccb35dc386a81764
bconstantine <bryan.constantine7@gmail.com>
2022-11-07 01:48:02 +0800

    test add

--- data_process/etth_data_loader.py
+++ data_process/etth_data_loader.py
@@ -232,13 +232,14 @@ class Dataset_Custom(Dataset):
         #num_test = int(len(df_raw)*0.2)
         #num_vali = len(df_raw) - num_train - num_test
 
-        num_test = 180+(6-(180-self.pred_len+1)%6) #180 - pred len(6) + 1 = 175, since batch size = 175 not completely divisible by batch size, 
+        num_test = 180+(6-(180-self.pred_len+1)%6)+1 #180 - pred len(6) + 1 = 175, since batch size = 175 not completely divisible by batch size, 
                                                    #we add 5 extra data, so that it becomes 185
                                                    #so that 185 - pred len(6) + 1 = 180, completely divisible by batch size (6)
         
         num_vali = 42+(6-(42-self.pred_len+1)%6)
         num_train = len(df_raw) - num_vali - num_test
 
+        #error: tuple index out of range
         #num_test = 180 + 6 - (180 % 6)
         #num_vali = 42 + 6 - (42 % 6)
         #num_train = len(df_raw)- num_test - num_vali
