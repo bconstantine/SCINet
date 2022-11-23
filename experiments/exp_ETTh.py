fecaec72eceea7deeaaa68abee31e26ee282ad40
bconstantine <bryan.constantine7@gmail.com>
2022-11-23 16:37:08 +0800

    upload new changes

--- experiments/exp_ETTh.py
+++ experiments/exp_ETTh.py
@@ -298,7 +298,7 @@ class Exp_ETTh(Exp_Basic):
                 in_dim = 18
             elif self.args.data == 'July_High_Correlation_Table_TEMP' or self.args.data == 'July_High_Correlation_Table_CUT_TEMP':
                 in_dim = 9
-            elif self.args.data == 'ALL_TEMP_WIND' or self.args.data == 'ALL_CUT_TEMP_WIND':
+            elif self.args.data == 'ALL_TEMP_WIND' or self.args.data == 'ALL_CUT_TEMP_WIND' or self.args.data == 'ALL_TEMP_WIND_OCTOBER' or self.args.data == 'ALL_TEMP_WIND_SEPTEMBER':
                 in_dim = 19
             elif self.args.data == 'July_High_Correlation_Table_TEMP_WIND' or self.args.data == 'July_High_Correlation_Table_CUT_TEMP_WIND':
                 in_dim = 10
@@ -420,6 +420,8 @@ class Exp_ETTh(Exp_Basic):
             'August_High_Correlation_TEMP': Dataset_Custom,
             'ALL_TEMP_OCTOBER': Dataset_Custom,
             'ALL_TEMP_SEPTEMBER':Dataset_Custom, 
+            'ALL_TEMP_WIND_OCTOBER': Dataset_Custom,
+            'ALL_TEMP_WIND_SEPTEMBER':Dataset_Custom, 
         }
         Data = data_dict[self.args.data]
         timeenc = 0 if args.embed!='timeF' else 1
